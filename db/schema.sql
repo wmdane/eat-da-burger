68e83d8be1941c080e326448b0b72171e5b81a38
Will Dane <enadlliw@gmail.com>
2020-12-11 17:44:53 -0500

    initial push plus files created

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id  INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Whopper", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Baconator", false);
